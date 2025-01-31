c1905ee9e632cfe124942301febc2dd8fadc74ee
Omkar Kshirsagar <omkarkshirsagar2005@gmail.com>
2025-01-31 23:41:09 +0530

    Pojo Class

---
+++ Ass_01_POJO_Class/src/com/tca/PojoClass.java
@@ -0,0 +1,35 @@
+package com.tca;
+
+public class PojoClass {
+	int rno;
+	String name;
+	double per;
+	
+	public PojoClass() {}
+
+	public int getRno() {
+		return rno;
+	}
+
+	public void setRno(int rno) {
+		this.rno = rno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPer() {
+		return per;
+	}
+
+	public void setPer(double per) {
+		this.per = per;
+	}
+	
+	
+}
