e0ac6eb0204eabf397da5d5a174acdaae88f6e1a
Omkar Kshirsagar <omkarkshirsagar2005@gmail.com>
2025-02-23 22:32:39 +0530

    try catch added

--- Ass_05_HIB_MYSQL_APP/src/main/java/com/tca/App.java
+++ Ass_05_HIB_MYSQL_APP/src/main/java/com/tca/App.java
@@ -12,30 +12,43 @@ public class App
     public static void main( String[] args )
     {
     	
-    	Configuration configuration = new Configuration();
-    	
-    	configuration.configure();
-    	 
-    	SessionFactory sessionFactory = configuration.buildSessionFactory();
-    	
-    	Session session = sessionFactory.openSession();
-    	
-    	Transaction transaction = session.beginTransaction();
+    	Configuration configuration = null;
+    	SessionFactory sessionFactory = null;
+    	Session session = null;
+    	Transaction transaction= null;
     	
     	
-    	Student student = new Student();
-    	
-    	student.setRno(102);
-    	student.setName("AAA");
-    	student.setPer(66.6);
-    	
-    	session.save(student);
-    	
-    	transaction.commit();
-    	session.close();
-    	sessionFactory.close();
-    	
-    	
-        System.out.println( "Data Saved Successfully" );
+    	try {
+	    	configuration = new Configuration();
+	    	
+	    	configuration.configure();
+	    	 
+	    	sessionFactory = configuration.buildSessionFactory();
+	    	
+	    	session = sessionFactory.openSession();
+	    	
+	    	transaction = session.beginTransaction();
+	    	
+	    	
+	    	Student student = new Student();
+	    	
+	    	student.setRno(101);
+	    	student.setName("AAA");
+	    	student.setPer(66.6);
+	    	
+	    	session.save(student);
+	    	
+	    	transaction.commit();
+	    	System.out.println( "Data Saved Successfully" );
+    	}
+    	catch(Exception e) {
+    		e.printStackTrace();
+    		transaction.rollback();
+    	}
+    	finally {
+    		session.close();
+        	sessionFactory.close();
+    	}
+    	    	
     }
 }
