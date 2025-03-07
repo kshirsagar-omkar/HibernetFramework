552d1173c65dc9ba9023665c7567353f3b3db75b
Omkar Kshirsagar <omkarkshirsagar2005@gmail.com>
2025-03-07 17:25:21 +0530

    addAnnotedClass

---
+++ Ass_12_Annotation_Demo/src/test/java/com/tca/ASS_12_Annotation_Demo/AppTest.java
@@ -0,0 +1,19 @@
+package com.tca.ASS_12_Annotation_Demo;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
